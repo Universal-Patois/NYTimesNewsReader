87411c2c99913d348b80d87a83484412dfb1f7bf
Andrew Knapick <ajknapick@yahoo.com>
2023-01-13 10:28:33 -0600

    Fix REAADME GIF

--- cypress/e2e/landing-page-spec.cy.js
+++ cypress/e2e/landing-page-spec.cy.js
@@ -1,9 +1,9 @@
 describe('landing-page', () => {
   beforeEach(() => {
-    cy.intercept('GET','https://api.nytimes.com/svc/topstories/v2/home.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
       fixture: "sample-articles.json"
     })
-    .visit("http://localhost:3000")
+      .visit("http://localhost:3000")
   })
 
   it('should have a header, dropdown, and list of top stories on the landing page', () => {
@@ -11,25 +11,23 @@ describe('landing-page', () => {
     cy.get('#dropdown-topic-button').contains('Choose a Topic')
     cy.get(':nth-child(2) > center').contains('Subjects: Classified Information and State Secrets. Search and Seizure. United States Politics and Government.')
   })
-}) 
+})
 
 describe('topic selection', () => {
-  beforeEach(() => {
-    beforeEach(() => {
-      cy.intercept('GET','https://api.nytimes.com/svc/topstories/v2/home.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
-        fixture: "sample-articles.json"
-      })
-      .visit("http://localhost:3000")
-      cy.get('#dropdown-topic-button').click()
-      cy.get('[value="automobiles"]').select()
-      cy.intercept('GET','https://api.nytimes.com/svc/topstories/v2/automobiles.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
-        fixture: "auto-articles.json"
-      })
-      .visit("http://localhost:3000")
-    })
 
-    it('should be able to select a topic from dropdown and have articles from that topic populate', () => {
-      cy.get(':nth-child(2) > center').click()
+  beforeEach(() => {
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
+      fixture: "sample-articles.json"
     })
+      .visit("http://localhost:3000")
+    cy.get('#dropdown-topic-button').click()
+    cy.get('[value="automobiles"]').click()
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/automobiles.json?api-key=cVmfn2b50bqSPs6mKHWXkxYuGAqWN2y4', {
+      fixture: "auto-articles.json"
+    })
+  })
+
+  it('should be able to select a topic from dropdown and have articles from that topic populate', () => {
+    cy.get(':nth-child(2) > center > a > .article-title').contains('How Biden’s Discovery of Classified Files Compares With the Trump Case')
   })
 })
